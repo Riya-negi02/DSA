2c3f8d8c0a84313fd1a3f45dfad8c99a04e18c38
Riya-negi02 <negiriya2005@gmail.com>
2026-03-04 21:21:30 +0530

    changes

---
+++ Bitmasking/01_bitmasking.cpp
@@ -0,0 +1,27 @@
+//playing with bits
+// #include<iostream>
+// using namespace std;
+// int count(int n){
+//     int count=0;
+//     while(n>0){
+//         count+= n&1;
+//         n >>=1;
+//     }
+//     return count;
+// }
+// int main(){
+//     int Q;
+//     cin>> Q;
+//     while(Q--){
+//         int a, b;
+//         cin>>a;
+//         cin>>b;
+//         int sum=0;
+//         for(int i=a; i<=b; i++){
+//             sum+= count(i);
+//         }
+//         cout<<sum;
+//     }
+//     return 0;
+// }
+
